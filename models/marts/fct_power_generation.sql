04452935992433d7bc7d83d13b691294b52ca181
Kevin Manoti <kevinnoty21@gmail.com>
2026-03-18 01:59:41 +0300

    add  marts model

--- models/marts/fct_power_generation.sql
+++ models/marts/fct_power_generation.sql
@@ -7,6 +7,14 @@ select
     year,
 
     capacity_mw,
-    generation_gwh_filled as generation_gwh
+
+    generation_gwh_final as generation_gwh,
+
+    -- 🔥 standout feature
+    case
+        when generation_gwh is not null then 'actual'
+        when estimated_generation_gwh is not null then 'estimated'
+        else 'imputed_avg'
+    end as generation_source
 
 from {{ ref('int_power_generation') }}
